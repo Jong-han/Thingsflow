0cc08f05e14caefa34d6fa6c2c0eb28ea0d2cf26
Jong-Han <jonghan9611@gmail.com>
2022-02-16 14:29:42 +0900

    gradle setting 및 packaging

--- app/src/main/java/com/jh/thingsflow/ui/main/MainActivity.kt
+++ app/src/main/java/com/jh/thingsflow/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.jh.thingsflow
+package com.jh.thingsflow.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jh.thingsflow.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
